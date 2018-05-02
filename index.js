0e00dbf0a0b071d81c3725b775eec9d404ed12d8
Felipe Sardim <felipesardim@gmail.com>
2018-05-02 09:06:56 -0300

    Lição de casa

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ const app = express();
 
 app.get('/', (request, response) => {
     response.status(200).send('API funcionando ok!');
-    // response.sendStatus(200);
+});
+app.get('/cep/:cep', (request, response) => {
+    const cep = request.params.cep;
+    //JSON completo da resposta da API do VIA CEP
+    //link: https://viacep.com.br/ws/<CEP DE CONSULTA>/json
+});
+app.get('/cep/:cep/rua', (request, response) => {
+    //Retornar nome da rua
+});
+app.get('/cep/:cep/bairro', (request, response) => {
+    //Retornar nome do bairro
 });
 
 app.listen(3000, () => {
